78e6427a2497b71eb258d8982ac734bf4b61a90a
Jan Kowalski <sino4@gazeta.pl>
2017-01-20 20:02:43 +0100

    Highscore

---
+++ Wave/Assets/Scripts/HighScore/HighScore.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighScore : MonoBehaviour {
+
+    const string privateCode = "T_QgWbagWkWpdaRi09dBVgJavH-TLEIk2wNezBo3y0Tg";
+    const string publicCode = "586fde84b6dd1500a4c45bda";
+    const string webURL = "http://dreamlo.com/lb/";
+
+    public GameObject HighscoreList;
+    public static string ScoreList;
+    public static string ScorePlayer;
+
+
+    public delegate void HighScoreAction();
+    public static event HighScoreAction EndDataDownload;
+
+    bool UploadingScore = false;
+
+    void Awake()
+    {
+        
+
+       
+    }
+
+    private void Start()
+    {
+        ScorePlayer = string.Empty;
+        ScoreList = string.Empty;
+        StartCoroutine(DownloadData());
+       
+    }
+
+    public void AddNewHighscore(float Time)
+    {
+
+        StartCoroutine(UploadNewHighscore(PlayerData.Nick, CodeHighscore(Time)));
+        
+    }
+
+    
+
+    IEnumerator UploadNewHighscore(string username, int score)
+    {
+        UploadingScore = true;
+        WWW www = new WWW(webURL + privateCode + "/add/" + WWW.EscapeURL(username) + "/" + score);
+        yield return www;
+        UploadingScore = false;
+        if (string.IsNullOrEmpty(www.error))
+            print("Upload Successful");
+        else
+        {
+            print("Error uploading: " + www.error);
+        }
+        
+    }
+
+    public static int CodeHighscore(float value)
+    {
+        return 1000000 - (int)Mathf.Round(value * 100);
+    }
+
+    public  static float UncodeHighscore(int value)
+    {
+        return (1000000 - value) / 100f;
+    }
+
+    public void RefreshData()
+    {
+        StartCoroutine(DownloadData());
+    }
+
+    IEnumerator DownloadData()
+    {
+        while (UploadingScore)
+        {
+            yield return new WaitForSeconds(0.1f);
+        }
+        WWW www = new WWW(webURL + publicCode + "/pipe/");
+        yield return www;
+        WWW www2 = new WWW(webURL + publicCode + "/pipe-get/" + PlayerData.Nick);
+        yield return www2;
+
+        if (!string.IsNullOrEmpty(www.error) && !string.IsNullOrEmpty(www2.error))
+        {
+
+            print("Error Downloading: " + www.error + " Player: " + www2.error);
+            
+        }
+        else
+        {
+            ScorePlayer = www2.text;
+            ScoreList = www.text;
+            EndDataDownload();
+        }
+        
+
+
+
+    }
+
+    
+
+
+
+}
+
---
+++ Wave/Assets/Scripts/HighScore/ShowHighScore.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ShowHighScore : MonoBehaviour {
+
+    const string privateCode = "T_QgWbagWkWpdaRi09dBVgJavH-TLEIk2wNezBo3y0Tg";
+    const string publicCode = "586fde84b6dd1500a4c45bda";
+    const string webURL = "http://dreamlo.com/lb/";
+
+    public GameObject System;
+
+    public Text LeftColumn;
+    public Text RightColumn;
+
+    public GameObject ButtonNextSite;
+    public GameObject ButtonPreviousSite;
+
+    
+
+    private void OnEnable()
+    {
+        ActualSite = 0;
+        HighScore.EndDataDownload += Refresh;
+    }
+
+    public int ActualSite;
+    bool LastSite = false;
+
+    public void Refresh()
+    {
+        string[] splited = HighScore.ScoreList.Split(new char[] { '|','\n' });
+        LastSite = false;
+        string forLeft = "";
+        string forRight = "";
+        const int JUMP = 6;
+        const int COUNT_SCORE_PER_SITE = 20;
+        for (int i = ActualSite * COUNT_SCORE_PER_SITE; i < (ActualSite+1)* COUNT_SCORE_PER_SITE ; i++)
+        {
+            if(i >= splited.Length / JUMP)
+            {
+                LastSite = true;
+                
+                break;
+            }
+            forLeft += (i + 1).ToString() + "." + splited[i* JUMP]  + "\n";
+            float ToGetMinutes = HighScore.UncodeHighscore(int.Parse(splited[i * JUMP + 1]));
+            int minutes = (int)ToGetMinutes / 60;
+            ToGetMinutes -= (minutes * 60);
+            forRight += minutes.ToString() + "." + ToGetMinutes.ToString() + "\n";
+            
+        }
+        
+        LeftColumn.text = forLeft;
+        RightColumn.text = forRight;
+        if(LastSite == true)
+        {
+            ButtonNextSite.SetActive(false);
+        }
+        else
+        {
+            ButtonNextSite.SetActive(true);
+        }
+        if(ActualSite == 0)
+        {
+            ButtonPreviousSite.SetActive(false);
+        }
+        else
+        {
+            ButtonPreviousSite.SetActive(true);
+        }
+        HighScore.EndDataDownload -= Refresh;
+    }
+    public void ChangeSite(int which)
+    {
+        if(HighScore.ScoreList == string.Empty)
+        {
+            return;
+        }
+        ActualSite = which;
+        Refresh();
+
+    }
+
+    public void SetNextSite()
+    {
+        ChangeSite(ActualSite + 1);
+
+    }
+
+    public void SetPreviousSite()
+    {
+        if(ActualSite == 0)
+        {
+            return;
+        }
+        ChangeSite(ActualSite - 1);
+
+    }
+    
+
+    void Update () {
+		
+	}
+}

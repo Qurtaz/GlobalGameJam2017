7205b5d3b4707fcc6ab11337e26d2a9262ee6a8a
Jan Kowalski <sino4@gazeta.pl>
2017-01-21 14:39:32 +0100

    Wave Data Load

---
+++ Wave/Assets/Scripts/MainGame/BuldingsData.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuldingsData : MonoBehaviour {
+
+    public const int MaxUpdateLevel = 3;
+    public const int CountofTypeBuilding = 3;
+    public static BuildingInfo[] Special = new BuildingInfo[CountofTypeBuilding];
+
+
+
+    void Awake()
+    {
+
+    }
+
+}
+
+
+public class BuildingInfo
+{
+    public Texture Icon;
+    public int BuildCost;
+    public int[] UpgradeCosts = new int[BuldingsData.MaxUpdateLevel];
+    public int[] ObstacleStrength = new int[BuldingsData.MaxUpdateLevel];
+
+    public BuildingInfo(Texture _icon, int[] _costs)
+    {
+        Icon = _icon;
+        for(int i = 0; i < BuldingsData.MaxUpdateLevel; i++)
+        {
+            ObstacleStrength[i] = 0;
+            UpgradeCosts[i] = _costs[i];
+        }
+    }
+
+    public BuildingInfo(Texture _icon, int[] _costs, int[] _obstacleStrength)
+    {
+        Icon = _icon;
+        for (int i = 0; i < BuldingsData.MaxUpdateLevel; i++)
+        {
+            ObstacleStrength[i] = _obstacleStrength[i];
+            UpgradeCosts[i] = _costs[i];
+        }
+    }
+
+
+}

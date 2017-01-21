20d5783e88c7caa163783cfd9b5ee97b5aeac84a
Jan Kowalski <sino4@gazeta.pl>
2017-01-21 23:07:24 +0100

    Generator 1/3

---
+++ Wave/Assets/Scripts/Building/Road.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Road : MonoBehaviour {
+
+    public GameObject[] Roads;
+
+	public void SetTypeRoad(int a)
+    {
+        for(int i = 0; i < Roads.Length; i++)
+        {
+            Roads[a].SetActive(false);
+        }
+        Roads[a].SetActive(true);
+    }
+}
--- Wave/Assets/Scripts/MainGame/Generator.cs
+++ Wave/Assets/Scripts/MainGame/Generator.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Generator : MonoBehaviour {
 
     public int length = 10;
-    public int width = 10;
+    public int width = 100;
 
     public int OffsetGeneratorX = 0;
     public int OffsetGeneratorZ = 0;
@@ -14,15 +14,55 @@ public class Generator : MonoBehaviour {
     public GameObject PrefabBuilding;
     public GameObject PrefabFoundation;
 
+    public int CountBuildingsT2 = 2;
+    public int CountBuildingsT1 = 4;
+
     public int Height = 2;
 
     void Start()
     {
+        Randomize();
         Generate();
+    }
+
+    int[] UpgradesBulid;
+    void Randomize()
+    {
+        UpgradesBulid = new int[(width - 4) * length / 2];
+        for (int i = 0; i < UpgradesBulid.Length; i++)
+        {
+            UpgradesBulid[i] = 0;
+        }
+
+        int ActualPosition = 0;
+        while(ActualPosition < CountBuildingsT2)
+        {
+            UpgradesBulid[ActualPosition] = 2;
+            ActualPosition++;
+        }
+        while (ActualPosition < CountBuildingsT2 + CountBuildingsT1)
+        {
+            UpgradesBulid[ActualPosition] = 1;
+            ActualPosition++;
+        }
+
+
+        int n = UpgradesBulid.Length;
+        while (n > 1)
+        {
+            n--;
+            int k = Random.Range(0,n + 1);
+            int value = UpgradesBulid[k];
+            UpgradesBulid[k] = UpgradesBulid[n];
+            UpgradesBulid[n] = value;
+        }
+
+        
     }
 
     void Generate()
     {
+        int CountOfHouse = 0;
         for(int i = 0; i < length; i++)
         {
             if(i%2 == 0)
@@ -59,7 +99,11 @@ public class Generator : MonoBehaviour {
                         }
                         else
                         {
-                            Instantiate(PrefabBuilding, GetPosition(i,j), transform.rotation);
+                            
+                            GameObject tmp;
+                            tmp = Instantiate(PrefabBuilding, GetPosition(i,j, -0.5f), transform.rotation) as GameObject;
+                            tmp.GetComponent<NormalBuilding>().SetLevelBuilding(UpgradesBulid[CountOfHouse]);
+                            CountOfHouse++;
                         }
                     }
                 }
@@ -71,5 +115,10 @@ public class Generator : MonoBehaviour {
     Vector3 GetPosition(int i, int j)
     {
         return new Vector3(j + OffsetGeneratorX, Height, i + OffsetGeneratorZ);
+    }
+
+    Vector3 GetPosition(int i, int j, float HeightModyfiaction)
+    {
+        return new Vector3(j + OffsetGeneratorX, Height + HeightModyfiaction, i + OffsetGeneratorZ);
     }
 }

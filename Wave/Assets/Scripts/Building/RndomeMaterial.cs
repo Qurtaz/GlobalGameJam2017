575d4e420dfb493cc383a62114b139f0e1142525
Qurtaz <piwowino1994@gamil.com>
2017-01-21 23:08:03 +0100

    fffff

---
+++ Wave/Assets/Scripts/Building/RndomeMaterial.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RndomeMaterial : MonoBehaviour {
+    public List<Material> materialList;
+    private MenagerBuilding men;
+	// Use this for initialization
+	void Start () {
+        int d  = (int)Random.Range(0, materialList.Count);
+        foreach(GameObject z in men.allFlors)
+        {
+            z.GetComponent<Renderer>().material = materialList[d];
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
